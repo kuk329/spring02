699154f6f5e20b9ef0819672f05ccb0a2ac9f78c
kuk329 <74480236+kuk329@users.noreply.github.com>
2023-02-09 22:59:37 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,14 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy= new FixDiscountPolicy();
+   // private final DiscountPolicy discountPolicy= new FixDiscountPolicy(); // 정액(고정) 할인 정책 적용
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 정률할인 정책 적용
+    // 문제점 : OrderServiceImpl 이 DiscountPolicy 인터페이스 뿐만아니라 FixDiscountPolicy 구현 클래스도 함께
+    // 의존하고 있다.
+   private DiscountPolicy discountPolicy;
+
+
+
 
 
     @Override
