c9954c0abc69d2b2c271bf7dbc08ec2bd63d67e0
kuk329 <74480236+kuk329@users.noreply.github.com>
2023-02-11 20:17:39 +0900

    IoC, DI, 컨테이너

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -12,6 +12,10 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig { // 공연 기획자 역할
     // 관심사의 분리 : 객체를 생성하고 연결하는 역할과 실행하는 역할이 명확히 분리되었다.
+    // IoC(Inversion of Control) : 제어의 역전
+     // AppConfig 가 등장한 이후에 구현 객체는 자신의 로직을 실행하는 역할만 담당한다. 프로그램의 제어 흐름은
+    // 이제 AppConfig가 가져간다. ex) OrderServiceImpl 은 필요한 인터페이스들을 호출하지만 어떤 구현 객체들이
+    // 실행될지 모른다.
 
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
